ff1c6771b883999c8a8cbbaf843013c5ff260664
sato0125 <ykh.2135027@stu.o-hara.ac>
2022-09-15 12:33:24 +0900

    268ページまで完成

--- diary/views.py
+++ diary/views.py
@@ -1,7 +1,7 @@
 import logging
 from django.urls import reverse_lazy
 from django.views import generic
-from .forms import InquiryForm
+from .forms import InquiryForm,DiaryCreateForm
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from.models import Diary
@@ -25,7 +25,30 @@ class InquiryView(generic.FormView):
 class DiaryListView(LoginRequiredMixin,generic.ListView):
     model = Diary
     template_name = 'diary_list.html'
+    #一つのページに何個日記を表示するか設定
+    paginate_by = 2
 
     def get_queryset(self):
         diaries = Diary.objects.filter(user=self.request.user).order_by('-created_at')
         return  diaries
+
+class DiaryDetailView(LoginRequiredMixin,generic.DetailView):
+    model = Diary
+    template_name = "diary_detail.html"
+
+class DiaryCreateView(LoginRequiredMixin,generic.CreateView):
+    model = Diary
+    template_name = 'diary_create.html'
+    form_class = DiaryCreateForm
+    success_url = reverse_lazy('diary:diary_list')
+
+    def form_valid(self, form):
+        diary = form.save(commit=False)
+        diary.user = self.request.user
+        diary.save()
+        messages.success(self.request,"日記を作成しました")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request,"日記の作成に失敗しました")
+        return super().form_valid(form)
